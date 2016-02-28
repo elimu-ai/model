135359644c87491154adb29a40b3296761a99b7e
Jo Grimstad <jogrimst@gmail.com>
2016-02-28 12:12:20 +0100

    #w1TGJlPr Added more entities

---
+++ src/main/java/org/literacyapp/model/json/Word.java
@@ -0,0 +1,36 @@
+package org.literacyapp.model.json;
+
+import org.literacyapp.model.enums.Language;
+
+public class Word {
+    
+    private Long id;
+    
+    private Language language;
+    
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
