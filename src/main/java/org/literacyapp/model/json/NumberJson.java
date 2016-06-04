be1bc19d5163b611306dd776b1c844d3e20a096d
Jo Grimstad <jo@literacyapp.org>
2016-06-04 17:33:23 +0200

    Added "Json" to class names

---
+++ src/main/java/org/literacyapp/model/json/NumberJson.java
@@ -0,0 +1,66 @@
+package org.literacyapp.model.json;
+
+import org.literacyapp.model.enums.Language;
+
+public class NumberJson {
+    
+    private Long id;
+    
+    private Language language;
+    
+    private Integer value;
+    
+    private String symbol;
+    
+    private WordJson word;
+    
+    private int[] dominantColor; // RGB array
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+    
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+    
+    public WordJson getWord() {
+        return word;
+    }
+
+    public void setWord(WordJson word) {
+        this.word = word;
+    }
+
+    public int[] getDominantColor() {
+        return dominantColor;
+    }
+
+    public void setDominantColor(int[] dominantColor) {
+        this.dominantColor = dominantColor;
+    }
+}
