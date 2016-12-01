628e7fca801be33c00c07c67d4ed8f2d97eec69e
Jo Grimstad <jo@literacyapp.org>
2016-12-01 11:27:55 +0300

    #67 Label multimedia content

--- src/main/java/org/literacyapp/model/gson/analytics/UsageEventGson.java
+++ src/main/java/org/literacyapp/model/gson/analytics/UsageEventGson.java
@@ -1,5 +1,7 @@
 package org.literacyapp.model.gson.analytics;
 
+import org.literacyapp.model.enums.content.LiteracySkill;
+import org.literacyapp.model.enums.content.NumeracySkill;
 import org.literacyapp.model.gson.StudentGson;
 import org.literacyapp.model.gson.admin.ApplicationGson;
 
@@ -8,6 +10,10 @@ public class UsageEventGson extends DeviceEventGson {
     private ApplicationGson application;
     
     private StudentGson student;
+    
+    private LiteracySkill literacySkill;
+    
+    private NumeracySkill numeracySkill;
 
     public ApplicationGson getApplication() {
         return application;
@@ -23,5 +29,21 @@ public class UsageEventGson extends DeviceEventGson {
 
     public void setStudent(StudentGson student) {
         this.student = student;
+    }
+
+    public LiteracySkill getLiteracySkill() {
+        return literacySkill;
+    }
+
+    public void setLiteracySkill(LiteracySkill literacySkill) {
+        this.literacySkill = literacySkill;
+    }
+
+    public NumeracySkill getNumeracySkill() {
+        return numeracySkill;
+    }
+
+    public void setNumeracySkill(NumeracySkill numeracySkill) {
+        this.numeracySkill = numeracySkill;
     }
 }
--- src/main/java/org/literacyapp/model/gson/content/multimedia/MultimediaGson.java
+++ src/main/java/org/literacyapp/model/gson/content/multimedia/MultimediaGson.java
@@ -1,9 +1,13 @@
 package org.literacyapp.model.gson.content.multimedia;
 
+import java.util.List;
 import java.util.Set;
 import org.literacyapp.model.enums.content.LiteracySkill;
 import org.literacyapp.model.enums.content.NumeracySkill;
 import org.literacyapp.model.gson.content.ContentGson;
+import org.literacyapp.model.gson.content.LetterGson;
+import org.literacyapp.model.gson.content.NumberGson;
+import org.literacyapp.model.gson.content.WordGson;
 
 public abstract class MultimediaGson extends ContentGson {
     
@@ -16,6 +20,12 @@ public abstract class MultimediaGson extends ContentGson {
     private Set<LiteracySkill> literacySkills;
     
     private Set<NumeracySkill> numeracySkills;
+    
+    private List<LetterGson> letters;
+    
+    private List<NumberGson> numbers;
+    
+    private List<WordGson> words;
 
     public String getDownloadUrl() {
         return downloadUrl;
@@ -55,5 +65,29 @@ public abstract class MultimediaGson extends ContentGson {
 
     public void setNumeracySkills(Set<NumeracySkill> numeracySkills) {
         this.numeracySkills = numeracySkills;
+    }
+
+    public List<LetterGson> getLetters() {
+        return letters;
+    }
+
+    public void setLetters(List<LetterGson> letters) {
+        this.letters = letters;
+    }
+
+    public List<NumberGson> getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(List<NumberGson> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<WordGson> getWords() {
+        return words;
+    }
+
+    public void setWords(List<WordGson> words) {
+        this.words = words;
     }
 }
