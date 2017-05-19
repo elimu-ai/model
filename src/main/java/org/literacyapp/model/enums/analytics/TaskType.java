c7c12b54f08965d2a5f376dbbe51437d445146db
Jo Grimstad <jo@literacyapp.org>
2017-05-19 19:06:43 +0200

    #55 Refactor TaskType

--- src/main/java/org/literacyapp/model/enums/analytics/TaskType.java
+++ src/main/java/org/literacyapp/model/enums/analytics/TaskType.java
@@ -1,8 +1,10 @@
-package org.literacyapp.model.enums.content;
+package org.literacyapp.model.enums.analytics;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import org.literacyapp.model.enums.content.LiteracySkill;
+import org.literacyapp.model.enums.content.NumeracySkill;
 
 public enum TaskType {
     
