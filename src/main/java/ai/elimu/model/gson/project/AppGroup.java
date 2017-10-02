3bbf5e64a8eab0206eff60cc8012cb0a2fac6b3b
Jo Grimstad <jo.grimstad@elimu.ai>
2017-10-02 22:26:41 +0200

    #133 Add AppGroup

--- src/main/java/ai/elimu/model/gson/project/AppGroup.java
+++ src/main/java/ai/elimu/model/gson/project/AppGroup.java
@@ -1,8 +1,18 @@
 package ai.elimu.model.gson.project;
 
 import ai.elimu.model.gson.BaseEntityGson;
+import ai.elimu.model.gson.admin.ApplicationGson;
+import java.util.List;
 
 public class AppGroup extends BaseEntityGson {
     
-    
+    private List<ApplicationGson> applications;
+
+    public List<ApplicationGson> getApplications() {
+        return applications;
+    }
+
+    public void setApplications(List<ApplicationGson> applications) {
+        this.applications = applications;
+    }
 }
