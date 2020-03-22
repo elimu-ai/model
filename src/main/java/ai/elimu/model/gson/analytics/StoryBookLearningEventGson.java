c7910e456d55a89870aac88aa34c11510f38e515
Nya Elimu <nya@elimu.ai>
2020-03-22 19:38:22 +0800

    #110 Add StoryBookLearningEvent

--- src/main/java/ai/elimu/model/gson/analytics/StoryBookLearningEventGson.java
+++ src/main/java/ai/elimu/model/gson/analytics/StoryBookLearningEventGson.java
@@ -1,42 +1,23 @@
 package ai.elimu.model.gson.analytics;
 
 import ai.elimu.model.enums.analytics.LearningEventType;
-import ai.elimu.model.gson.BaseEntityGson;
-import ai.elimu.model.gson.admin.ApplicationGson;
-import ai.elimu.model.gson.content.StoryBookGson;
 import java.util.Calendar;
 
 /**
  * For documentation, see https://github.com/elimu-ai/webapp/tree/master/src/main/java/ai/elimu/model
  */
-public class StoryBookLearningEventGson extends BaseEntityGson {
-    
-    private String androidId;
-    
-    private ApplicationGson application;
+public class StoryBookLearningEventGson {
     
     private Calendar time;
     
+    private String androidId;
+    
+    private String packageName;
+    
+    private Long storyBookId;
+    
     private LearningEventType learningEventType;
     
-    private StoryBookGson storyBook;
-    
-    public String getAndroidId() {
-        return androidId;
-    }
-
-    public void setAndroidId(String androidId) {
-        this.androidId = androidId;
-    }
-
-    public ApplicationGson getApplication() {
-        return application;
-    }
-
-    public void setApplication(ApplicationGson application) {
-        this.application = application;
-    }
-
     public Calendar getTime() {
         return time;
     }
@@ -45,19 +26,35 @@ public class StoryBookLearningEventGson extends BaseEntityGson {
         this.time = time;
     }
 
+    public String getAndroidId() {
+        return androidId;
+    }
+
+    public void setAndroidId(String androidId) {
+        this.androidId = androidId;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+    
+    public Long getStoryBookId() {
+        return storyBookId;
+    }
+
+    public void setStoryBookId(Long storyBookId) {
+        this.storyBookId = storyBookId;
+    }
+
     public LearningEventType getLearningEventType() {
         return learningEventType;
     }
 
     public void setLearningEventType(LearningEventType learningEventType) {
         this.learningEventType = learningEventType;
-    }
-
-    public StoryBookGson getStoryBook() {
-        return storyBook;
-    }
-
-    public void setStoryBook(StoryBookGson storyBook) {
-        this.storyBook = storyBook;
     }
 }
