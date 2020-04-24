1652a94f89d97a183902ef434656bbef0ea4b8a1
Nya Elimu <nya@elimu.ai>
2020-04-24 09:54:31 +0800

    #178 Add WordLearningEvent

--- src/main/java/ai/elimu/model/enums/analytics/LearningEventType.java
+++ src/main/java/ai/elimu/model/enums/analytics/LearningEventType.java
@@ -3,5 +3,6 @@ package ai.elimu.model.enums.analytics;
 public enum LearningEventType {
     
     STORYBOOK_OPENED,
-    STORYBOOK_COMPLETED
+    STORYBOOK_COMPLETED,
+    WORD_IN_STORYBOOK_PRESSED
 }
