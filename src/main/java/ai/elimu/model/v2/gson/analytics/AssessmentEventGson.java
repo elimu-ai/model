edb7d88fae1b29f8b2891d86c735b724b966ec1b
Nya Elimu <nya@elimu.ai>
2020-05-20 16:40:57 +0800

    #187 Add WordAssessmentEvent

---
+++ src/main/java/ai/elimu/model/v2/gson/analytics/AssessmentEventGson.java
@@ -0,0 +1,40 @@
+package ai.elimu.model.v2.gson.analytics;
+
+import ai.elimu.model.v2.gson.BaseEntityGson;
+import java.util.Calendar;
+
+/**
+ * For documentation, see https://github.com/elimu-ai/webapp/tree/master/src/main/java/ai/elimu/model
+ */
+public abstract class AssessmentEventGson extends BaseEntityGson {
+    
+    private Calendar time;
+    
+    private String androidId;
+    
+    private String packageName;
+    
+    public Calendar getTime() {
+        return time;
+    }
+
+    public void setTime(Calendar time) {
+        this.time = time;
+    }
+
+    public String getAndroidId() {
+        return androidId;
+    }
+
+    public void setAndroidId(String androidId) {
+        this.androidId = androidId;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+}
---
+++ src/main/java/ai/elimu/model/v2/gson/analytics/WordAssessmentEventGson.java
@@ -0,0 +1,47 @@
+package ai.elimu.model.v2.gson.analytics;
+
+/**
+ * For documentation, see https://github.com/elimu-ai/webapp/tree/master/src/main/java/ai/elimu/model
+ */
+public class WordAssessmentEventGson extends LearningEventGson {
+    
+    private Long wordId;
+    
+    private String wordText;
+    
+    private Float masteryScore;
+
+    private Long timeSpentMs;
+
+    public Long getWordId() {
+        return wordId;
+    }
+
+    public void setWordId(Long wordId) {
+        this.wordId = wordId;
+    }
+
+    public String getWordText() {
+        return wordText;
+    }
+
+    public void setWordText(String wordText) {
+        this.wordText = wordText;
+    }
+
+    public Float getMasteryScore() {
+        return masteryScore;
+    }
+
+    public void setMasteryScore(Float masteryScore) {
+        this.masteryScore = masteryScore;
+    }
+
+    public Long getTimeSpentMs() {
+        return timeSpentMs;
+    }
+
+    public void setTimeSpentMs(Long timeSpentMs) {
+        this.timeSpentMs = timeSpentMs;
+    }
+}
