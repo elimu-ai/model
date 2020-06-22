d94e89fb50d1a3a2874e3ed56787e0f96f765045
Nya Elimu <nya@elimu.ai>
2020-06-22 16:49:10 +0800

    #199 Add LetterToAllophoneMappingGson

--- src/main/java/ai/elimu/model/v2/gson/content/WordGson.java
+++ src/main/java/ai/elimu/model/v2/gson/content/WordGson.java
@@ -1,14 +1,17 @@
 package ai.elimu.model.v2.gson.content;
 
 import ai.elimu.model.enums.content.WordType;
+import java.util.List;
 
 /**
  * For documentation, see https://github.com/elimu-ai/webapp/tree/master/src/main/java/ai/elimu/model
  */
 public class WordGson extends ContentGson {
     
-    @Deprecated // TODO: replace with list of Letters
+    @Deprecated // TODO: replace with LetterToAllophoneMappings
     private String text;
+    
+    private List<LetterToAllophoneMappingGson> letterToAllophoneMappings;
     
     private WordType wordType;
 
@@ -28,5 +31,13 @@ public class WordGson extends ContentGson {
 
     public void setWordType(WordType wordType) {
         this.wordType = wordType;
+    }
+
+    public List<LetterToAllophoneMappingGson> getLetterToAllophoneMappings() {
+        return letterToAllophoneMappings;
+    }
+
+    public void setLetterToAllophoneMappings(List<LetterToAllophoneMappingGson> letterToAllophoneMappings) {
+        this.letterToAllophoneMappings = letterToAllophoneMappings;
     }
 }
