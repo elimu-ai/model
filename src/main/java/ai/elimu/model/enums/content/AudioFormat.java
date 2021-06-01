577ad8bf96e7695313b53972ac1d2c97e30e3c25
Nya <info@elimu.ai>
2021-06-01 13:19:30 +0200

    #237 Add contentType to AudioFormat enum

--- src/main/java/ai/elimu/model/enums/content/AudioFormat.java
+++ src/main/java/ai/elimu/model/enums/content/AudioFormat.java
@@ -1,8 +1,23 @@
 package ai.elimu.model.enums.content;
 
 public enum AudioFormat {
+    
+    OGG("audio/ogg"),
+    MP3("audio/mpeg"),
+    WAV("audio/wave");
+    
+    /**
+     * The MIME type - https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+     * <p>
+     * Example: "audio/mpeg"
+     */
+    private String contentType;
+    
+    private AudioFormat(String contentType) {
+        this.contentType = contentType;
+    }
 
-    OGG,
-    MP3,
-    WAV
+    public String getContentType() {
+        return contentType;
+    }
 }
