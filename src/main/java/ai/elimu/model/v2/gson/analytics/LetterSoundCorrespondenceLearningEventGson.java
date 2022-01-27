a31f385cfc0e61db7f44e25be45bf657f9a09397
Nya <info@elimu.ai>
2022-01-28 01:55:30 +0800

    feat: Add LetterSoundCorrespondenceLearningEventGson

---
+++ src/main/java/ai/elimu/model/v2/gson/analytics/LetterSoundCorrespondenceLearningEventGson.java
@@ -0,0 +1,17 @@
+package ai.elimu.model.v2.gson.analytics;
+
+/**
+ * For documentation, see https://github.com/elimu-ai/webapp/tree/master/src/main/java/ai/elimu/model
+ */
+public class LetterSoundCorrespondenceLearningEventGson extends LearningEventGson {
+    
+    private Long letterSoundCorrespondenceId;
+
+    public Long getLetterSoundCorrespondenceId() {
+        return letterSoundCorrespondenceId;
+    }
+
+    public void setLetterSoundCorrespondenceId(Long letterSoundCorrespondenceId) {
+        this.letterSoundCorrespondenceId = letterSoundCorrespondenceId;
+    }
+}
