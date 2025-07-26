476b18c116732aa0a21ebc13a19a2b7469d73722
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-07-26 11:19:38 +0700

    refactor: use string list in letter-sound learning events

--- src/main/java/ai/elimu/model/v2/gson/analytics/LetterSoundLearningEventGson.java
+++ src/main/java/ai/elimu/model/v2/gson/analytics/LetterSoundLearningEventGson.java
@@ -1,18 +1,31 @@
 package ai.elimu.model.v2.gson.analytics;
 
+import java.util.List;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
- * For documentation, see https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/entity
+ * Based on https://github.com/elimu-ai/webapp/blob/main/src/main/java/ai/elimu/entity/analytics/LetterSoundLearningEvent.java
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class LetterSoundLearningEventGson extends LearningEventGson {
 
-  private String[] letterSoundLetters;
+    /**
+     * The sequence of letters. E.g. <code>["s","h"]</code>.
+     */
+    private List<String> letterSoundLetters;
 
-  private String[] letterSoundSounds;
+    /**
+     * The sequence of sounds (IPA values). E.g. <code>["ʃ"]</code>.
+     */
+    private List<String> letterSoundSounds;
 
-  private Long letterSoundId;
+    /**
+     * This field might not be included, e.g. if the assessment task was done in a 
+     * 3rd-party app that did not load the content from the elimu.ai Content Provider. 
+     * In that case, this field will be {@code null}.
+     */
+    private Long letterSoundId;
 }
